fa600506c676fa100195cace92ce341a3e98bc79
梅昊睿 <145778769+meihaorui@users.noreply.github.com>
2024-05-01 11:52:12 +0800

    Add files via upload

---
+++ 朋友录.py
@@ -0,0 +1,10 @@
+friend=['胡昊钧','王笑一','黄正一','刘伯僮','徐江平','勒晋嘉']
+print(friend)
+print(friend[0]+'是我损友。')
+print(friend[1]+'也是我损友。')
+print(friend[2]+'是我朋友。')
+print(friend[3]+'也是我朋友。')
+print(friend[4]+'还是我朋友。')
+print(friend[5]+'是个傻逼(朋友)。')
+friend2={'胡昊钧':'损友','王笑一':'损友','黄正一':'朋友','刘伯僮':'朋友','徐江平':'朋友','勒晋嘉':'傻逼'}
+print(friend2)
